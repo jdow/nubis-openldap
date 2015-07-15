ac20159eccbcde07fbb7806e5438f1ba84f4880c
Justin Dow <justin@dowhaus.com>
2015-07-15 10:25:19 -0700

    removing bin directory

--- bin/ssh-rhel.sh
+++
@@ -1,2 +0,0 @@
-STACK_NAME="$1"
-ssh -A -t ec2-user@jumphost.sandbox.us-west-2.nubis.allizom.org "ssh -A -t ec2-user@$(nubis-consul --stack-name $STACK_NAME --settings nubis/cloudformation/parameters.json get-ec2-instance-ip)"
---
+++ nubis/puppet/openldap.pp
@@ -0,0 +1,1 @@
+class { 'openldap::server': }
