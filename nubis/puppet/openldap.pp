b50c8333c5835de7e6a84843bbc1abbcba089dbd
limed <limed@mozilla.com>
2015-09-11 15:56:52 -0700

    Added additional scripts

--- nubis/puppet/openldap.pp
+++ nubis/puppet/openldap.pp
@@ -1,4 +1,13 @@
-package {
-    'openldap-servers':
-        ensure => latest;
+package { 'openldap-servers':
+    ensure => latest;
+}
+
+file { '/etc/openldap/certs':
+    ensure  => directory,
+    require => Package['openldap-servers']
+}
+
+file { '/etc/openldap/cacerts':
+    ensure  => directory,
+    require => Package['openldap-servers']
 }
