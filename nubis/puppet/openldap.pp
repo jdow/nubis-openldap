b84590ddb21f434f8a2386a383e89416f5a48304
limed <limed@mozilla.com>
2015-09-17 16:47:53 -0700

    Copying some stuff from moco's openldap module so that it matches up with what we have

--- nubis/puppet/openldap.pp
+++ nubis/puppet/openldap.pp
@@ -20,3 +20,48 @@ file { '/etc/openldap/restart-ldap.sh':
     source  => 'puppet:///nubis/files/ldap/restart-ldap.sh',
     require => Package['openldap-servers']
 }
+
+file { '/var/lib/ldap_access_logs':
+    ensure  => directory,
+    owner   => 'ldap',
+    group   => 'ldap',
+    recurse => true,
+    require => Package['openldap-servers']
+}
+
+file { '/var/lib/ldap_access_logs/logs':
+    ensure  => directory,
+    owner   => 'ldap',
+    group   => 'ldap',
+    require => File['/var/lib/ldap_access_logs']
+}
+
+file { '/var/lib/ldap/logs':
+    ensure  => directory,
+    owner   => 'ldap',
+    group   => 'ldap',
+    require => Package['openldap-servers']
+}
+
+file { '/var/lib/ldap/auditlogs':
+    ensure  => directory,
+    owner   => 'ldap',
+    group   => 'ldap',
+    require => Package['openldap-servers']
+}
+
+file { '/var/lib/ldap_access_logs/DB_CONFIG':
+    ensure  => file,
+    owner   => 'ldap',
+    group   => 'ldap',
+    source  => 'puppet:///nubis/files/ldap/DB_CONFIG',
+    require => File['/var/lib/ldap_access_logs'],
+}
+
+file { '/var/lib/ldap/DB_CONFIG':
+    ensure  => file,
+    owner   => 'ldap',
+    group   => 'ldap',
+    source  => 'puppet:///nubis/files/ldap/DB_CONFIG',
+    require => Package['openldap-servers'],
+}
