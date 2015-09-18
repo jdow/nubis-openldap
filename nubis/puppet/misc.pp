8cd8fb02fb32ba67171b8703fb6cee98e6bbae21
limed <limed@mozilla.com>
2015-09-18 16:45:45 -0700

    Added a service check

--- nubis/puppet/misc.pp
+++ nubis/puppet/misc.pp
@@ -12,3 +12,19 @@ file { '/etc/nubis.d/99-who-be-the-leader':
     target  => '/usr/local/bin/nubis-startup/who-be-the-leader',
     require => File['/usr/local/bin/nubis-startup/who-be-the-leader']
 }
+
+file { '/usr/local/bin/check_ldapmaster_leader':
+    ensure  => link,
+    target  => '/usr/local/bin/nubis-startup/who-be-the-leader',
+    require => File['/usr/local/bin/nubis-startup/who-be-the-leader']
+}
+
+file { '/etc/consul/svc-ldapmaster-leader.json':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///nubis/files/svc-ldapmaster-leader.json',
+    require => [ File['/usr/local/bin/nubis-startup/who-be-the-leader'], File['/usr/local/bin/check_ldapmaster_leader'] ]
+}
+
